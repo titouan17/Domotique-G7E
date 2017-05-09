95062bd3344dbdc58a6e760e58dad5f6375ed9d4
NathanGeffroy <nathangeffroy@outlook.fr>
2017-05-09 22:55:18 +0200

    Add files via upload

--- header2.php
+++ header2.php
@@ -1,20 +1,37 @@
-
 <header id="header2">
 	<div id="conteneur">
-		<div id="conteneur_header2">
-			<a href="Domotique.php"><img class="logo" src="photoSite/maisonvrai4.png" alt="logo"/></a>
-			<div class="nom">
-				<h1>G7 Entreprise</h1>
-			</div>
+		<a href="Domotique.php"><img class="logo" src="photoSite/maisonvrai4.png" alt="logo"/></a>
+		<div class="nom">
+			G7 Entreprise
 		</div>
 	</div>
 
-	<nav>
+	<form>
+		<input type="text" name="search" placeholder="Rechercher..." class="rechercher">
+	</form>
+
+	<nav class="head2">
 		<ul>
 			<li><a href="Domotique.php">Accueil</a></li>
-			<li><a href="#">Contact</a></li>
-			<li><a href="monDomicile.php">Mon domicile</a></li>
-			<li><a href="monProfil.php">Mon Profil</a></li>
+			<li><a href="contact.php">Contact</a></li>
+			<li>
+				<div class="dropdown">
+					<a href="monDomicile.php">Mon domicile</a>
+					<div class="dropdown-content">
+						<a href="#">Domicile</a>
+						<a href="#">Pièces</a>
+						<a href="#">Capteurs</a>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="dropdown">
+					<a href="monProfil.php">Mon Profil</a>
+					<div class="dropdown-content">
+						<a href="#">Paramètres</a>
+						<a href="#">Déconnexion</a>
+					</div>
+				</div></li>
 		</ul>
 	</nav>
 </header>
