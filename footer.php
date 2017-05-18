69ff8443bceba9fcfda034989bcb9eaf65deb850
NathanGeffroy <nathangeffroy@outlook.fr>
2017-05-18 12:51:56 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,22 @@
 <footer>
-		<div id="conteneurFooter">
-			<div class="copyright">Copyright G7Enterprise - Tous droits réservés</div>
-			<div class="contact"><a href="#">Nous contacter</a></div>
+		<div id="copyright">
+			<h1>G7Enterprise</h1>
+			<p>Tous droits réservés</p>
 		</div>
+
+		<div id="tweet">
+			<h1>Nous suivre</h1>
+
+			<p><img src="Image/Facebook.png" alt="Facebook" /><img src="Image/tweeter.png" alt="Twitter"><img src="Image/flickr.png" alt="Flickr"><img src="Image/Pinterest.png" alt="Pinterest"></p>
+		</div>
+
+		<div id="propos">
+			<h1>A propos de nous</h1>
+			<ul>
+				<li><a href="contact.php">Contact</a></li>
+				<li><a href="QuiSommesNous.php">Qui sommes-nous ?</a></li>
+				<li><a href="CeMAC.php">Nos produits</a></li>
+				<li><a href="#">Demander une installation</a></li>
+			</ul>
+		</div>			
 </footer>
