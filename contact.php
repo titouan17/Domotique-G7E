c3aad6df95af5f1b5086fe9eef195b6a3d5eb679
NathanGeffroy <nathangeffroy@outlook.fr>
2017-05-18 22:20:28 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="Style/domotique1.css">
+		<title>G7Entreprise</title>
+    </head>
+
+<body>
+
+	<?php include'header2.php';?> 
+	
+	<section>
+		<div id="contact-box">
+			<h1>Contact</h1>
+
+			<div id="contact">
+				<div class="b1">
+					<h3>Besoin d'un conseil?</h3>			
+					<p>Vous avez une question ou vous désirez plus d'informations concernant les services ou produits de G7Entreprise ?
+						Nos conseillers, formés régulièrement sur tous les nouveaux produits, vous offrent une réelle expertise.</p></br>
+					<a href="#" class="appeler">Nous contacter pour plus d'informations</a>
+				</div>
+
+				<div class="b1">
+					<h3>Problème de fonctionnement</h3>							
+					<p>Notre aide en ligne vous apportera des solutions à vos problèmes, des conseils d'entretien, des listes d'accessoires pour profiter pleinement de votre appareil.</p>
+					<br/>
+					<br/>
+					<a href="#">réserver une intervention en ligne</a>
+				</div>
+
+
+				<div class="b1">
+					<h3>Vous souhaitez faire une réclamation ?</h3>	
+					<p>Vous n’êtes pas satisfait et souhaitez que G7Entreprise vous apporte une solution. Pour nous permettre de vous répondre rapidement, voici les moyens pour nous contacter :
+					<ul>
+						<li>g7entreprise@gmail.com</li>
+						<li>06 56 78 95 02</li>
+						<li>Service Consommateurs G7Entreprise - 10 rue de Vanves 92310 Issy Les Moulineaux</li>
+					</ul>
+					</p>
+				</div>		
+			</div>
+
+			<div id="team">
+				<h1>L'equipe</h1>
+				<div class="tête">
+					<figure>
+						<img src="Image/bebe.jpg" alt="Nathan Geffroy">
+						<figcaption>Nathan Geffroy Co-fondateur</figcaption>
+					</figure>
+
+					<figure>
+						<img src="Image/bebe.jpg" alt="Halil Meral">
+						<figcaption>Halil Meral Co-fondateur</figcaption>
+					</figure>
+
+					<figure>
+						<img src="Image/bebe.jpg" alt="Matthieu">
+						<figcaption>Matthieu Ramognino Co-fondateur</figcaption>
+					</figure>
+
+					<figure>
+						<img src="Image/bebe.jpg" alt="Titouan">
+						<figcaption>Titouan Florentin Co-fondateur</figcaption>
+					</figure>
+					
+					<figure>
+						<img src="Image/bebe.jpg" alt="Wallerand">
+						<figcaption>Wallerand d'Estaintot Co-fondateur</figcaption>
+					</figure>
+
+					<figure>
+						<img src="Image/bebe.jpg" alt="Raphaël">
+						<figcaption>Raphaël Prevost Co-fondateur</figcaption>
+					</figure>
+				</div>	
+			</div>
+		</div>
+	</section>
+
+	<?php include 'footer.php'; ?>
+</body>
