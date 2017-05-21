3c2b44ff49bc3a60a9927e83ee67cb6a3e0c35ae
NathanGeffroy <nathangeffroy@outlook.fr>
2017-05-21 15:53:44 +0200

    Update monProfil.php

--- monProfil.php
+++ monProfil.php
@@ -18,30 +18,39 @@ session_start();
 			<aside>
 				<div id="profil">
 					<div class="description_profil">
-						<img src="	" alt="Photo de profil" >
+						<img src="" alt="Photo de profil" >
 						<h1><?php echo ($_SESSION['prenom'] . ' ' . $_SESSION['nom']); ?><h1>
 						<p>Adresse domicile 1: <?php echo($_SESSION['rue'] . ' ' . $_SESSION['codePostal'] . ' ' . $_SESSION['ville']); ?> <br/>
 						Téléphone portable: <?php echo ($_SESSION['telephone']); ?> <br/>
 						Date de Naissance: <?php echo ($_SESSION['date']); ?> 
 						</p>
-						<a href="monDomicile.php?cible=monDomicile" class="equiper">Ajouter un domicile</a>
-						<a href="#" class="equiper">Ajouter un utilisateur à votre domicile</a>
-						<a href="#" class="equiper">Ajouter un capteur</a>
+						<a href="#C2" class="equiper">Ajouter un domicile</a>
+						<a href="#C1" class="equiper">Ajouter un utilisateur à votre domicile</a>
+						<a href="index.php?cible=monDomicile" class="equiper">Ajouter un capteur</a>
 					</div>	
 				</div>
 			</aside>
 
 			<div id="informations">
 				<div class="description_informations">
-					<h2>Domiciles enregistrés</h2>
-					<p>Adresse : <?php echo ($_SESSION['rue'] . ' ' . $_SESSION['codePostal'] . ' ' . $_SESSION['ville']);	?></p>
-					<p>Surface : </p>
-					<p>Nombre de capteurs : </p>
-					<button class="equiper">Modifier</button>
+					<h2 id="C2">Domiciles enregistrés</h2>
+					<p><?php echo ('Domicile 1 : ' . ' ' . $_SESSION['rue'] . ' ' . $_SESSION['codePostal'] . ' ' . $_SESSION['ville']);?></p>
+
+					<form method="POST" action="">
+						<p class="form">
+							<label for="Adresse">Adresse : </label><input type="text" name="rue" id="rue" placeholder="Rue" />							
+							<input type="text" name="codePostal" placeholder="Code postal" />
+							<input type="text" name="ville" id="ville" placeholder="Ville" />
+							<input type="text" name="pays" placeholder="Pays" />
+							<br/>
+							<br/>
+							<input type="submit" name="Ajouter" />
+						</p>
+					</form>	
 				</div>
 				
 				<div class="description_informations">
-					<h2>Utilisateurs</h2>
+					<h2 id="C1">Utilisateurs</h2>
 					<p>Utilisateurs enregistrés sur votre domicile et ayant le contrôle des capteurs</p>
 					<ul>
 						<li>Utilisateur1</li>
@@ -49,7 +58,33 @@ session_start();
 						<li>Utilisateur3</li>
 						<li>Utilisateur4</li>
 					</ul>
-					<button class="equiper">Modifier</button>
+
+					<form method="POST" action="">
+						<p class="form">
+							<label for="Adresse">Adresse : </label>
+							<input type="text" name="rue" id="rue" placeholder="Rue" />
+							<input type="text" name="codePostal" id="codePostal" placeholder="Code Postal" />
+							<input type="text" name="ville" id="ville" placeholder="Ville" />
+							<input type="text" name="pays" placeholder="Pays" />
+						</p>
+					</form>
+					<form method="POST" action="">
+						<p>
+							<label for="prenom">Prénom : </label>
+							<input type="text" name="prenom" placeholder="Prenom" />
+							<br/>
+							<br/>
+							<label for="prenom">Nom : </label>
+							<input type="text" name="nom" placeholder="Nom" />
+							<br/>
+							<br/>
+							<label for="email">Email : </label>
+							<input type="email" name="email" placeholder="Email" />
+							<br/>
+							<br/>
+							<input type="submit" name="Ajouter" />
+						</p>
+					</form>
 				</div>
 			</div>
 		</section>
