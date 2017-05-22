c2108076d8367b3ef9ed97f16476290b19d71595
halil-ibrahimme <Halil-ibrahim.meral@hotmail.fr>
2017-05-22 11:41:14 +0200

    Update MonDomicile.php

--- MonDomicile.php
+++ MonDomicile.php
@@ -7,7 +7,7 @@ session_start();
     <head>
         <meta charset="utf-8" />
         <link rel="stylesheet" href="/php/App/Matthieu/Git/Style/Domotique.css">
-		<title>G7Entreprise</title>
+		<title>Domisep</title>
     </head>
 
 <body>
