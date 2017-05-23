6d884e6996aad9029f86870033e5584023c9cbc1
halil-ibrahimme <Halil-ibrahim.meral@hotmail.fr>
2017-05-23 10:30:56 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,39 @@
+<footer>
+	<div id="copyright">
+		<h1>Domisep</h1>
+		<p>Tous droits réservés</p>
+		<h1>Adresse</h1>
+		<p>10 Rue de Vanves 92130 ISSY LES MOULINEAUX</p>
+	</div>
+
+	<div id="tweet">
+		<h1>Nous suivre</h1>
+
+		<p><img src="Image/Facebook.png" alt="Facebook" /><img src="Image/tweeter.png" alt="Twitter"><img src="Image/flickr.png" alt="Flickr"><img src="Image/Pinterest.png" alt="Pinterest"></p>
+	</div>
+
+	<div id="propos">
+		<h1>À propos de nous</h1>
+		<ul>
+			<li><a href="contact.php">Contact</a></li>
+			<li><a href="CeMAC.php">Nos produits</a></li>
+			<li><a href="#">Demander une installation</a></li>
+		</ul>
+	</div>	
+
+	<div>
+		<h1>Mentions légales</h1>
+		<p>Le présent site est édité par la société Domisep:</p>
+
+		<ul>
+			<li>Adresse :10 Rue de Vanves 92130 ISSY LES MOULINEAUX</li>
+			<li>Immatriculation : RCS Paris 812 328 631</li>
+			<li>Directeur de la publication: M. Jérémie Eskenazi.</li>
+			<li>Capital social : 10 000 €</li>
+
+		</ul>
+
+		<p>L’hébergement du site est assuré par OVH,<br/> société par actions simplifiée dont le siège social est <br/> au 2 rue Kellermann, 59100 Roubaix et dont le numéro de téléphone est le 0820 698 765. </p>
+	</div>
+
+</footer>
