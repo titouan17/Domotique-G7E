2cba31e7830e8a7764eee157a8d3365c88268ad7
halil-ibrahimme <Halil-ibrahim.meral@hotmail.fr>
2017-05-24 10:31:33 +0200

    Update header1.php

--- header1.php
+++ header1.php
@@ -2,7 +2,7 @@
 <header id="header2">
 	<div id="conteneur">
 		<div id="conteneur_header2">
-			<a href="Domotique.php"><img class="logo" src="photoSite/maisonvrai4.png" alt="logo"/></a>
+			<a href="Domotique.php"><img class="logo" src="photoSite/maison1.png" alt="logo"/></a>
 			<div class="nom">
 				<h1>Domisep</h1>
 			</div>
