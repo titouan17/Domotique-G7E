5e4d1ca1bcbf82092c5cea83dda21436deda7098
NathanGeffroy <nathangeffroy@outlook.fr>
2017-06-06 16:57:47 +0200

    Page pour la FAQ (manuel utilisateur)

---
+++ contenuAide.php
@@ -0,0 +1,44 @@
+<section>
+	<div id="aide">
+		<div class="description_aide">
+			<h1>Que pouvons-nous faire pour vous ?</h1>
+
+			<h2>Inscription et identification</h2>
+			<ul>		
+				<h3>Je ne parviens pas à me connecter</h3>
+				<p><!-- Emplcement paragraphe --></p>
+				
+
+				<h3>Modifier ou réinitialiser son mot de passe</h3>
+				<h3>Adresse mail déjà utilisée</h3>
+				<h3>Comment fermer un compte ?</h3>
+			</ul>
+
+
+
+			<h2>Comment gérer mon profil</h2>
+				<ul>
+					<h3>Modifier mon profil</h3>
+					<h3>Comment ajouter un utilisateur à mon domicile ?</h3>
+				</ul>
+
+
+
+			<h2>Gérer ma maison</h2>
+				<ul>
+					<h3>Comment ajouter une pièce ?</h3>
+					<h3>Comment ajouter un capteur ?</h3>
+					<h3>Comment accéder aux informations d'une pièce ou d'un capteur ?</h3>
+					<h3>Comment afficher mes capteurs par type ou par pièce ?</h3>
+				</ul>
+
+
+
+			<h2>Contacter Domisep</h2>
+				<ul>
+					<h3>Comment contacter Domisep ?</h3>
+					<h3>Mon mail n'a pas été envoyé</h3>
+				</ul>
+		</div>
+	</div>	
+</section>
