8906cb46a4ed0404e83764170b40d4f70856bb8e
NathanGeffroy <nathangeffroy@outlook.fr>
2017-06-12 09:17:25 +0200

    Update contenuAide.php

--- contenuAide.php
+++ contenuAide.php
@@ -2,42 +2,93 @@
 	<div id="aide">
 		<div class="description_aide">
 			<h1>Que pouvons-nous faire pour vous ?</h1>
+			<div id="sommaire">
+				<div class="sommaire_colonne">
+					<h2><a href="#inscription_identification" class='h2'>Inscription et identification</a></h2>
+						<ul>
+							<h4><a href="#1.1" class='h4'>Je ne parviens pas à me connecter</a></h4>
+							<h4><a href="#1.2" class='h4'>Modifier ou réinitialiser son mot de passe</a></h4>
+							<h4><a href="#1.3" class='h4'>Adresse mail déjà utilisée</a></h4>
+							<h4><a href="#1.4" class='h4'>Comment fermer un compte ?</a></h4>
+						</ul>
+					<h2><a href="#Gerer_mon_profil" class='h2'>Gérer mon profil</a></h2>
+						<ul>
+							<h4><a href="#2.1" class='h4'>Modifier mon profil</a></h4>
+							<h4><a href="#2.2" class='h4'>Ajouter un utilisateur à mon domicile</a></h4>
+						</ul>
+				</div>
 
-			<h2>Inscription et identification</h2>
-			<ul>		
-				<h3>Je ne parviens pas à me connecter</h3>
-				<p><!-- Emplcement paragraphe --></p>
-				
+				<div class="sommaire_colonne">
+					<h2><a href="#Gerer_ma_maison" class='h2'>Gérer ma maison</a></h2>
+						<ul>
+							<h4><a href="#3.1" class='h4'>Ajouter une pièce</a></h4>
+							<h4><a href="#3.2" class='h4'>Ajouter un capteur</a></h4>
+							<h4><a href="#3.3" class='h4'>Informations d'une pièce ou d'un capteur</a></h4>
+							<h4><a href="#3.4" class='h4'>Afficher mes capteurs par type ou par pièce</a></h4>
+						</ul>
+					<h2><a href="#Contacter_Domisep" class='h2'>Contacter Domisep</a></h2>
+						<ul>
+							<h4><a href="#4.1" class='h4'>Contacter Domisep</a></h4>
+							<h4><a href="#4.2" class='h4'>Mon mail n'a pas été envoyé</a></h4>
+						</ul>
+				</div>
+			</div>
 
-				<h3>Modifier ou réinitialiser son mot de passe</h3>
-				<h3>Adresse mail déjà utilisée</h3>
-				<h3>Comment fermer un compte ?</h3>
-			</ul>
+			<h2 id="inscription_identification">Inscription et identification</h2>
+				<ul>		
+					<h3 id="1.1">Je ne parviens pas à me connecter</h3>
+					<p>Si vous n'arrivez pas à vous connectez, hésitez pas contact Domisep afin d'avoir plus d'information concernant votre compte.</p><br/>
+					
+					<h3 id="1.2">Modifier ou réinitialiser son mot de passe</h3>
+					<p>Si vous avez oublié votre mot de passe, sélectionnez la page Mon Profil et réinitialiser votre mot de passe en cliquant sur "modifier mon profil".</p><br/>
+					
+					<h3 id="1.3">Adresse mail déjà utilisée</h3>
+					<p>Dans certains cas, un utilisateur déjà inscrit utilise déjà l'adresse mail entrée lors de l'inscription. Utilisez une autre adresse mail ou créez-en une nouvelle afin de valider votre inscription.</p><br/>
+					
+					<h3 id="1.4">Comment fermer un compte ?</h3>
+					<p>Vous ne souhaitez plus utiliser les produits de Domisep ? Contactez-nous afin de fermer votre compte.</p><br/>
+				</ul>
 
-
-
-			<h2>Comment gérer mon profil</h2>
+			<h2 id="Gerer_mon_profil">Gérer mon profil</h2>
 				<ul>
-					<h3>Modifier mon profil</h3>
-					<h3>Comment ajouter un utilisateur à mon domicile ?</h3>
+					<h3 id="2.1">Modifier mon profil</h3>
+					<p>Si vous souhaitez modifier vos paramètres personnels, sur la page <a href="index.php?cible=monProfil">Mon Profil</a>, sélectionnez le bouton "Modifier mon profil" et remplissez les formulaires que vous souhaitez modifier. Les formulaires n'ayant pas été modifiés ne seront pas pris en compte et vos informations seront inchangées.</p><br/>
+					
+					<h3 id="2.2">Ajouter un utilisateur à mon domicile</h3>
+					<p>Vous pouvez ajouter un utilisateur déjà inscrit sur Domisep à votre domicile. Pour cela, sur la page <a href="index.php?cible=monProfil">Mon Profil</a> entrez les informations nécessaires dans le cadre "Ajouter un utilisateur".</p><br/>
 				</ul>
 
 
 
-			<h2>Gérer ma maison</h2>
+			<h2 id="Gerer_ma_maison">Gérer ma maison</h2>
 				<ul>
-					<h3>Comment ajouter une pièce ?</h3>
-					<h3>Comment ajouter un capteur ?</h3>
-					<h3>Comment accéder aux informations d'une pièce ou d'un capteur ?</h3>
-					<h3>Comment afficher mes capteurs par type ou par pièce ?</h3>
+					<h3 id="3.1">Ajouter une pièce</h3>
+					<p>Rendez-vous sur la page <a href="index.php?cible=monDomicile">Mon Domicile</a> afin de contrôler votre maison. Pour ajouter une pièce, cliquez, sur le menu de gauche, "Ajouter une pièce" et entrez le nom de la pièce que vous souhaitez ajouter. Un fenêtre apparaîtra sur votre écran, confirmant l'ajout de la pièce.</p><br/>
+
+					<h3 id="3.2">Ajouter un capteur</h3>
+					<p>Toujours sur <a href="index.php?cible=monDomicile">Mon Domicile</a>, cliquez sur le bouton "ajouter un capteur" du menu de gauche. Remplissez les paramètres demandés tels que le type de capteur à ajouter et l'endroit voulu. Un capteur apparaîtra à l'endroit sélectionné.</p><br/>
+					
+					<h3 id="3.3">Informations d'une pièce ou d'un capteur</h3>
+					<p>Afin d'accéder aux données des capteurs, cliquez dessus et agissez de la même manière pour une pièce. En effet, en sélectionnant une salle, les informations associées s'afficheront ainsi que les capteurs présent à cet endroit.</p><br/>
+					
+					<h3 id="3.4">Afficher mes capteurs par type ou par pièce</h3>
+					<p>Sélectionnez l'affichage des capteurs par type afin de faire apparaître tous les types de capteurs que vous possédez dans votre domicile. Ensuite, vous pouvez les sélectionner pour visualiser les pièces dans lesquels ils sont présents.</p>
+					<p>L'affichage par pièce est sélectionné par défaut, cependant, apres avoir choisi un autre type d'affichage, vous pouvez le retrouver quand bon vous semble en choisissant "Par pièce".</p><br/>
 				</ul>
 
 
 
-			<h2>Contacter Domisep</h2>
+			<h2 id="Contacter_Domisep">Contacter Domisep</h2>
 				<ul>
-					<h3>Comment contacter Domisep ?</h3>
-					<h3>Mon mail n'a pas été envoyé</h3>
+					<h3 id="4.1">Contacter Domisep</h3>
+					<p>Rendez-vous sur la page <a href="index.php?cible=Contact">Contact</a>, un formulaire de contact devrait apparaître. Remplissez tous les formulaires et envoyez votre message à Domisep en cliquant sur le bouton "Envoyer". Le mail sera alors transmi au service client de Domisep et vous répondra le plus rapidement possible.</p>
+					
+					<h3 id="4.2">Mon mail n'a pas été envoyé</h3>
+					<ul>
+						<li><p>Verifiez que vous avez rempli tous les formulaires</p></li>
+						<li><p>Les informations entrées peuvent être incorrectes</p></li>
+						<li><p>Contactez DOMISEP par téléphone au 0676656565 si le problème persiste.</p></li>
+					</ul>
 				</ul>
 		</div>
 	</div>	
